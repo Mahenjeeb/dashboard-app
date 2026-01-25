b637a294632d4c9b972f49a5580265b82a420755
Mahenjeeb <mahenjeeb345@gmail.com>
2026-01-25 15:02:11 +0530

    Removed app.listen() for vercel

--- server/src/server.js
+++ server/src/server.js
@@ -9,17 +9,24 @@ const app = express();
 dotenv.config();
 
 // CORS middleware should be before other middlewares
-app.use(cors({ 
-  origin: process.env.CLIENT_URL, 
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 connectDatabase(process.env.MONGODB_URL);
 app.use("/api", userRouter);
 app.use("/api", appRouter);
-app.listen(process.env.PORT, () => {
-  console.log(`Server Listneting on Port ${process.env.PORT} 🚀`);
+app.get("/", (_, resp) => {
+  resp.send("Server Online");
 });
+if (process.env.NODE_ENV != "production") {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server Listneting on Port ${process.env.PORT} 🚀`);
+  });
+}
