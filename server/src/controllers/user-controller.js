f79ad245c80c9c384851e8d6fa0eda69d9441825
Mahenjeeb <mahenjeeb345@gmail.com>
2026-01-26 14:48:38 +0530

    Removed domain from controller

--- server/src/controllers/user-controller.js
+++ server/src/controllers/user-controller.js
@@ -6,8 +6,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const cookieOptions = {
   httpOnly: true,
   secure: isProd,
-  sameSite: isProd ? 'none' :'lax',
-  domain: isProd ? '.vercel.app' : 'localhost'
+  sameSite: isProd ? 'none' :'lax'
 };
 
 const signUpUser = async (req, resp) => {
