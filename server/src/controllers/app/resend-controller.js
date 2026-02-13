04812d926489004621ceb2ddca7833100c41bb91
Mahenjeeb <mahenjeeb345@gmail.com>
2026-02-13 10:59:31 +0530

    added awit for resend controller

--- server/src/controllers/app/resend-controller.js
+++ server/src/controllers/app/resend-controller.js
@@ -2,12 +2,14 @@ import { Resend } from 'resend';
 export const sendMail = async (to, html) => {
   try {
     const resend = new Resend(process.env.RESEND_API_KEY);
-    resend.emails.send({
+    const data = await resend.emails.send({
       from: 'onboarding@resend.dev',
       to,
       subject: "App Invitation",
       html,
     });
+    console.log('Email sent successfully:', data.id);
+    return data;
   } catch (error) {
     console.error("Mail error:", error);
     throw error;
