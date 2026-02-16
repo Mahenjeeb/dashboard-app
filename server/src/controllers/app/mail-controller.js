0787c510465eae9817f861bb4353b67cff810a48
Mahenjeeb <mahenjeeb345@gmail.com>
2026-02-16 11:39:11 +0530

    To avoid timeout

--- server/src/controllers/app/mail-controller.js
+++ server/src/controllers/app/mail-controller.js
@@ -1,8 +1,10 @@
 import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
